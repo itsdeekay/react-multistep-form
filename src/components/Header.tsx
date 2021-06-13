4d0638630ac6ddcab7288108ed66e00eaa4e3013
Deepak Kumar <deepjindl65@gmail.com>
2021-06-13 19:13:16 +0530

    Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="title">
+      Vahak
+          </div>
+    </div>
+  );
+}
+
+export default Header;
